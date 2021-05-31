31e512d4347bceb38f908622a1655516b7886f33
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2021-05-31 10:22:29 +0200

    string calcutor kata solution

---
+++ MyStringCalculatorKata/StringCalculatorKata/StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace StringCalculatorKata.Test
+{
+  [TestFixture]
+  public class StringCalculatorTest
+  {
+    [Test]
+    public void GivenNoNumbers_ShouldReturn0()
+    {
+      // Arrange
+      const string input = "";
+      const int expected = 0;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Add(input);
+      // Assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Given1Numbers_ShouldReturnSameNumber()
+    {
+      // Arrange
+      const string input = "5";
+      const int expected = 5;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Add(input);
+      // Assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenNumbersCommaDelimited_ShouldReturnBeSummed()
+    {
+      // Arrange
+      const string input = "5,10";
+      const int expected = 15;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Add(input);
+      // Assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenNumbersNewLineDelimited_ShouldReturnBeSummed()
+    {
+      // Arrange
+      const string input = "11\n13";
+      const int expected = 24;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Add(input);
+      // Assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenNumbersDelimitedAnyway_ShouldReturnBeSummed()
+    {
+      // Arrange
+      const string input = "11\n13,2%4";
+      const int expected = 30;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Add(input);
+      // Assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenNegativeInput_ShouldThrow()
+    {
+      // Arrange
+      const string input = "-1";
+      const string expected = "Negatives not allowed";
+      var sut = CreateSut();
+      // Act
+      var result = Assert.Throws<InvalidOperationException>(() =>
+      {
+        sut.Add(input);
+      });
+      // Assert
+      if (result != null) Assert.AreEqual(expected, result.Message);
+    }
+
+    [Test]
+    public void GivenNumbersGreaterThan1000_ShouldBeIgnored()
+    {
+      // Arrange
+      const string input = "11\n13,2%4,1200,1500,1300";
+      const int expected = 30;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Add(input);
+      // Assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    private static StringCalculator CreateSut()
+    {
+      var sut = new StringCalculator();
+      return sut;
+    }
+  }
+}
---
+++ MyStringCalculatorKata/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculatorKata
+{
+  public class StringCalculator
+  {
+    public int Add(string input)
+    {
+      var numbers = input.Split(new char[] {',', '\n', '%' });
+
+      if (string.IsNullOrEmpty(input))
+      {
+        return 0;
+      }
+
+      return numbers.Length > 1 ? GetSum(numbers) : ConvertStringToInt(input);
+    }
+
+    private static int GetSum(IEnumerable<string> numbers)
+    {
+      var sum = 0;
+
+      foreach (var number in numbers)
+      {
+        if (ConvertStringToInt(number) > 1000)
+        {
+          continue;
+        }
+
+        sum += ConvertStringToInt(number);
+      }
+
+      return sum;
+    }
+
+    private static int ConvertStringToInt(string input)
+    {
+      var num = int.Parse(input);
+
+      if (num < 0)
+      {
+        throw new InvalidOperationException("Negatives not allowed");
+      }
+
+      return num;
+    }
+  }
+}
