c3f33c0ddd5efc5410f94237d703dfb097261a90
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2021-09-13 15:33:14 +0200

    added a new version of age calculator

---
+++ MyAgeCalculatorKata/My Own Version/AgeCalculatorKata/AgeCalculatorKata.Test/AgeCalculatorTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AgeCalculatorKata.Test
+{
+  [TestFixture]
+  public class AgeCalculatorTest
+  {
+    [Test]
+    public void GivenIAmBornToday_ShouldReturn0()
+    {
+      // Arrange
+      var dateOfBirth = new DateTime(2021, 01, 01);
+      var targetDate = new DateTime(2021, 01, 01);
+      const int expected = 0;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Calculate(dateOfBirth, targetDate);
+      // assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenMyBirthdayIsToday_ShouldIncrementAgeBy1()
+    {
+      // Arrange
+      var dateOfBirth = new DateTime(1993, 01, 01);
+      var targetDate = new DateTime(2021, 01, 01);
+      const int expected = 28;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Calculate(dateOfBirth, targetDate);
+      // assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenMyBirthdayIsTomorrow_ShouldReturnCurrentAge()
+    {
+      // Arrange
+      var dateOfBirth = new DateTime(1993, 01, 02);
+      var targetDate = new DateTime(2021, 01, 01);
+      const int expected = 27;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Calculate(dateOfBirth, targetDate);
+      // assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenMyBirthdayWasYesterday_ShouldIncrementAgeBy1()
+    {
+      // Arrange
+      var dateOfBirth = new DateTime(1993, 01, 01);
+      var targetDate = new DateTime(2021, 01, 02);
+      const int expected = 28;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Calculate(dateOfBirth, targetDate);
+      // assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenMyBirthdayIsInAMonthFromNow_ShouldReturnCurrentAge()
+    {
+      // Arrange
+      var dateOfBirth = new DateTime(1993, 02, 01);
+      var targetDate = new DateTime(2021, 01, 01);
+      const int expected = 27;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Calculate(dateOfBirth, targetDate);
+      // assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenMyBirthdayWasAMonthAgo_ShouldIncrementAgeBy1()
+    {
+      // Arrange
+      var dateOfBirth = new DateTime(1993, 01, 01);
+      var targetDate = new DateTime(2021, 02, 01);
+      const int expected = 28;
+      var sut = CreateSut();
+      // Act
+      var actual = sut.Calculate(dateOfBirth, targetDate);
+      // assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GivenIAmYetToBeBorn_ShouldThrow()
+    {
+      //Arrange
+      var dateOfBirth = new DateTime(2022, 06, 15);
+      var targetDate = new DateTime(2021, 01, 01);
+      const string expected = "Cannot calculate age, the given birthday means the person hasn't been born yet.";
+      var sut = CreateSut();
+      //Act
+      var actual = Assert.Throws<Exception>(() =>
+      {
+        sut.Calculate(dateOfBirth, targetDate);
+      });
+      //Assert
+      Assert.AreEqual(expected, actual.Message);
+    }
+
+    private static AgeCalculator CreateSut()
+    {
+      var sut = new AgeCalculator();
+      return sut;
+    }
+  }
+}
---
+++ MyAgeCalculatorKata/My Own Version/AgeCalculatorKata/AgeCalculatorKata/AgeCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AgeCalculatorKata
+{
+  public class AgeCalculator
+  {
+    public int Calculate(DateTime dateOfBirth, DateTime today)
+    {
+      var age = CalculateAge(dateOfBirth, today);
+
+      if (age < 0)
+      {
+        throw new Exception("Cannot calculate age, the given birthday means the person hasn't been born yet.");
+      }
+
+      return age;
+    }
+
+    private static int CalculateAge(DateTime dateOfBirth, DateTime today)
+    {
+      var age = today.Year - dateOfBirth.Year;
+
+      if (today.Month >= dateOfBirth.Month)
+      {
+        if (today.Day < dateOfBirth.Day)
+        {
+          --age;
+        }
+      }
+      else
+      {
+        --age;
+      }
+
+      return age;
+    }
+  }
+}
