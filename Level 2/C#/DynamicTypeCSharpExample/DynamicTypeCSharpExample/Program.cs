c395a4734e553f6f847392edfae86ff7bcbbf5ad
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2023-03-06 08:34:22 +0200

    added new project to demo the dynamic type in c#

---
+++ Level 2/C#/DynamicTypeCSharpExample/DynamicTypeCSharpExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DynamicTypeCSharpExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            dynamic value1 = 10;
+            dynamic value2 = "hello";
+
+            Console.WriteLine("Value 1 is of type: " + value1.GetType());
+            Console.WriteLine("Value 2 is of type: " + value2.GetType());
+            Console.WriteLine("");
+
+            value1 = "world";
+
+            Console.WriteLine("Value 1 is now of type: " + value1.GetType());
+
+            value2 = 3.14;
+
+            Console.WriteLine("Value 2 is now of type: " + value2.GetType());
+        }
+    }
+}
