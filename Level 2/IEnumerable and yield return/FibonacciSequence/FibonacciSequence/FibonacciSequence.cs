ecd2f1f6598631993de53d0753e345fed6edf9e1
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2023-03-06 08:11:31 +0200

    added a new project to demo the C# IEnumarable and yield return

---
+++ Level 2/IEnumerable and yield return/FibonacciSequence/FibonacciSequence/FibonacciSequence.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace FibonacciSequence
+{
+    public class FibonacciSequence : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            int num1 = 0, num2 = 1;
+            while (true)
+            {
+                yield return num1;
+                var tempNum = num1;
+                num1 = num2;
+                num2 = tempNum + num2;
+            }
+        }
+    }
+}
---
+++ Level 2/IEnumerable and yield return/FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FibonacciSequence
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fibonacciSequence = new FibonacciSequence();
+            foreach (int index in fibonacciSequence)
+            {
+                if (index > 1000)
+                    break;
+                Console.WriteLine(index);
+            }
+        }
+    }
+}
